af16fdb29632c66c8fdc0dc3cdecaafe7db5dde0
chayeoi <chayeoikeem@gmail.com>
2019-03-16 21:56:42 +0900

    feat: Add Heading component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import { Heading } from './components'
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <Heading />
       </div>
     )
   }
---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Heading.module.css'
+
+const Heading = () => (
+  <h1 className={styles.title}>
+    The Great
+    <em className="">RGB</em>
+    Guessting Challenge
+  </h1>
+)
+
+export default Heading
