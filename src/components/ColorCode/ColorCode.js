ecccc25b3a82725a739e3294633e3dc6173f5518
chayeoi <chayeoikeem@gmail.com>
2019-03-17 11:29:41 +0900

    style: Change mutliple lines to single line

--- src/components/ColorCode/ColorCode.js
+++ src/components/ColorCode/ColorCode.js
@@ -2,9 +2,7 @@ import React from 'react'
 import styles from './ColorCode.module.css'
 
 const ColorCode = ({ answer }) => (
-  <p className={styles.answer}>
-    {answer.color}
-  </p>
+  <p className={styles.answer}>{answer.color}</p>
 )
 
 export default ColorCode
